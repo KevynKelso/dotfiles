8c0a216abcfea592da59e12e0ecf28fbf11f461b
Kevyn Kelso <kevynkelso@outlook.com>
2023-08-15 21:17:29 -0600

    feat(zshrc): add syntax highlighting

--- setup.sh
+++ setup.sh
@@ -39,6 +39,7 @@ if !(which zsh); then
     # pure theme
     mkdir -p "$HOME/.zsh"
     git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 # Install pyenv
